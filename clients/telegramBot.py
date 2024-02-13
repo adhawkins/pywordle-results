a75b16f1102987c7fcb19939558869a022ce3e0f
Andy Hawkins <andy@gently.org.uk>
2024-02-13 12:45:27 +0000

    Lookup a user based on telegram info, create / update if necessary
    Fixes #16

--- clients/telegramBot.py
+++ clients/telegramBot.py
@@ -59,6 +59,87 @@ def removeGroup(group):
     return None
 
 
+def findUserWithTelegramID(telegramID):
+    url = f"{base_url}users?telegramid={telegramID}"
+
+    response = requests.get(url, auth=basicAuth)
+    if response:
+        foundUsers = response.json()["users"]
+        if len(foundUsers) == 1:
+            return foundUsers[0]
+
+    return None
+
+
+def findUserWithFullName(fullName):
+    url = f"{base_url}users?fullname={fullName}"
+
+    response = requests.get(url, auth=basicAuth)
+    if response:
+        foundUsers = response.json()["users"]
+        if len(foundUsers) == 1:
+            return foundUsers[0]
+
+    return None
+
+
+def setUserTelegramID(user, telegramID):
+    print(f"Setting telegram ID for {user} to {telegramID}")
+
+    userUpdate = {
+        "username": user["username"],
+        "telegram_id": telegramID,
+        "fullname": user["fullname"],
+    }
+
+    response = requests.patch(
+        f"{base_url}users/{user['id']}", auth=basicAuth, json=userUpdate
+    )
+
+    if response:
+        return response.json()["user"]
+
+    return None
+
+
+def createUser(userName, telegramID, fullName):
+    print(f"Creating user: {userName}, '{fullName}' - {telegramID}")
+
+    user = {
+        "username": userName,
+        "telegram_id": telegramID,
+        "fullname": fullName,
+    }
+
+    response = requests.post(f"{base_url}users", auth=basicAuth, json=user)
+
+    if response:
+        return response.json()["user"]
+
+    return None
+
+
+def getUser(userName, telegramID, fullName):
+    # Check if a user exists with that telegram ID, if it does, use it
+    user = findUserWithTelegramID(telegramID)
+    if user:
+        return user
+
+    # If not, find a user whose full name matches
+    user = findUserWithFullName(fullName)
+    if user:
+        # If that user doesn't have a telegram ID, update it and use it
+        if user["telegram_id"] == 0:
+            user = setUserTelegramID(user, telegramID)
+            return user
+        else:
+            return None
+
+    # Otherwise, create the user
+
+    return createUser(userName, telegramID, fullName)
+
+
 async def botMembership(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.my_chat_member.chat.type == ChatType.GROUP:
         message = "What happened?"
@@ -86,6 +167,23 @@ async def botMembership(update: Update, context: ContextTypes.DEFAULT_TYPE):
             )
 
 
+async def groupMembership(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print("groupMembership")
+
+    userName = update.chat_member.new_chat_member.user.username
+    if not userName:
+        userName = f"{update.chat_member.new_chat_member.user.first_name} {update.chat_member.new_chat_member.user.last_name}".lower().replace(
+            " ", ""
+        )
+
+    user = getUser(
+        userName,
+        update.chat_member.new_chat_member.user.id,
+        f"{update.chat_member.new_chat_member.user.first_name} {update.chat_member.new_chat_member.user.last_name}",
+    )
+    print(f"Found user: {user['username']}")
+
+
 @click.command()
 @click.pass_context
 def telegramBot(ctx):
@@ -101,5 +199,10 @@ def telegramBot(ctx):
         botMembership, ChatMemberHandler.MY_CHAT_MEMBER
     )
     application.add_handler(botMembershipHandler)
+
+    groupMembershipHandler = ChatMemberHandler(
+        groupMembership, ChatMemberHandler.CHAT_MEMBER
+    )
+    application.add_handler(groupMembershipHandler)
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
